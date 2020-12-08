da7467e01c7e153923f05d1f0873f8390ab488c5
chiennt1024 <33412532+chiennt1024@users.noreply.github.com>
2020-12-08 13:17:41 +0700

    Update bt812s.rb

--- bt812s.rb
+++ bt812s.rb
@@ -1,3 +1,14 @@
-#bt1
+#bt1 Cho String str = "hello". Uppercase từ đầu tiên của string trên
 str = "hello"
 str.capitalize
+
+#bt2 Tỉnh tổng của các phần tử từ 20 tới 50
+$sum = 0
+for num in 20..50
+  $sum+= num
+end
+puts $sum
+
+#bt3 Điểm khác biệt giữa 1 and "1"? 1 la number "1" la string
+
+#bt4  Cho các phần tử từ 0 tới 10. In ra các phần tử chẵn(sử dụng  hàm step)
