58a281855516ab82ad9e41070d315143859da9ec
chiennt1024 <33412532+chiennt1024@users.noreply.github.com>
2020-12-08 17:48:24 +0700

    Update str.rb

--- str.rb
+++ str.rb
@@ -1,1 +1,22 @@
-
+# concatenating strings
+puts "Hello" + "Ec Ec"
+# string interpolation
+car = "Dream"
+puts "I like #{car}"
+# string manipulation
+puts "I have a pen. Apple pen".upcase
+puts "I have a pen. Apple pen".downcase
+puts "I have a pen. Apple pen".capitalize
+puts "I have a pen. Apple pen".swapcase
+puts "I have a pen. Apple pen".reverse
+puts "I have a pen. Apple pen".reverse.upcase
+# string substitution
+str = "the quick brown fox jumps over the lazy dog"
+str.sub('e', '?')
+str.gsub('e', '?')
+# spit and strip methods
+str = "the quick brown fox jumps over the lazy dog"
+str.split
+str.split(' ', 4)
+str2 = "   the quick brown fox jumps over    the lazy dog      "
+str2.strip
