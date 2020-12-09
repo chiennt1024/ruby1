aef6eb7b98cb11575878f2195c8a247f4db808ad
chiennt1024 <33412532+chiennt1024@users.noreply.github.com>
2020-12-09 09:54:08 +0700

    Create bt912s.rb

---
+++ bt912s.rb
@@ -0,0 +1,14 @@
+#Bài tập 1: dùng hàm select lấy ra phần tử chia hết cho 2 của mảng a = [2,3,4,6,7]
+#Bài tập 2: Dùng hàm inject tính tổng của mảng các  phần tử từ 20 tới 50
+#Bài tập 3: Chạy câu lệnh sau và giải thích kết quả
+#40 > 100 ? "Greater than" : "Less than"
+#Bài tập 1: Tạo 1 mảng gồm các phần tử sau: "tao", "chuoi", "cam"
+#Bài tập 2: In ra phần tử đầu tiên của mảng
+#                In ra phần tử cuối cùng của mảng
+#                Thêm một phần tử "sua chua" vào mảng
+#Bài tập 3: Tạo ra 1 mảng với 3 cặp key/value gồm mã và tên đầy đủ của các sân bay. Ví duj: mã HN của Sân bay Nội Bài. 
+#               In ra màn hình "Sân bay Nội Bài ở HN". Tương tự với các cặp khác
+#Bài tập 4: Cho mảng a = [1, 2, 3,4,5,6,7,2,3,1,2,3,1,4,6,7,4,6]. Trả ra mảng mà các phần tử unique và số lần xuất hiện của phần tử đó.
+#Bài tập 5: Cho mảng animals = ["cat", "dog", "tiger"]
+#In ra màn hình tên con vật với chữ cái đầu được viết hoa và số thứ tự của nó.
+#VD: "Cat vị trí số 1"
